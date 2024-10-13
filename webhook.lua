040c32c10704f87fd13be77e03083b90c117a7d5
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-10-13 14:04:12 +1100

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,2 +1,1 @@
-local Url = 'https://discord.com/api/webhooks/1294605510649188384/4_nKy26wHgDCZlJSyBsMmhEqNYakEvc4jXzyqSHvOr6D1Ul4_7sPPxkm7I49sYdHh7fa';
-return Url;
+return 'https://discord.com/api/webhooks/1294605510649188384/4_nKy26wHgDCZlJSyBsMmhEqNYakEvc4jXzyqSHvOr6D1Ul4_7sPPxkm7I49sYdHh7fa';
